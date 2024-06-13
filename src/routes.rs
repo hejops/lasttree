60bf7a95eaf48aec83e2a68df77891064cc5e4b7
Joseph <hejops1@gmail.com>
2024-06-13 10:24:15 +0100

    return Markup instead of HttpResponse

--- src/routes.rs
+++ src/routes.rs
@@ -1,9 +1,6 @@
 use actix_web::get;
-use actix_web::http::header::ContentType;
 use actix_web::web;
-use actix_web::HttpRequest;
-use actix_web::HttpResponse;
-use anyhow::Context;
+use maud::html;
 use maud::Markup;
 
 use crate::error_500;
@@ -11,34 +8,26 @@ use crate::ArtistTree;
 use crate::SqPool;
 
 #[get("/")]
-pub async fn home() -> Result<HttpResponse, actix_web::Error> {
-    let html = "hello world";
-    Ok(HttpResponse::Ok()
-        .content_type(ContentType::html())
-        .body(html))
+pub async fn home() -> actix_web::Result<Markup> {
+    let html = html! {"hello world"};
+    Ok(html)
 }
 
 #[get("/artists")]
-pub async fn search_artist() -> Result<HttpResponse, actix_web::Error> {
-    let html = "search artist:";
-    Ok(HttpResponse::Ok()
-        .content_type(ContentType::html())
-        .body(html))
+pub async fn search_artist() -> actix_web::Result<Markup> {
+    let html = html! {"search artist:"};
+    Ok(html)
 }
 
 #[get("/artists/{artist}")]
 pub async fn get_artist(
-    req: HttpRequest,
+    path: web::Path<String>,
     pool: web::Data<SqPool>,
 ) -> actix_web::Result<Markup> {
-    let artist = req
-        .match_info()
-        .get("artist")
-        .context("no artist supplied")
-        .map_err(error_500)?;
+    let artist = path.into_inner();
 
-    let tree = ArtistTree::new(artist, &pool).await;
+    let tree = ArtistTree::new(&artist, &pool).await;
+
     let html = tree.as_html().await.map_err(error_500)?;
-
     Ok(html)
 }
