c6146dfec5923ff01cade4430881d19ea02943cc
Joseph <hejops1@gmail.com>
2024-07-04 11:10:56 +0100

    add src/utils.rs

---
+++ src/utils.rs
@@ -0,0 +1,39 @@
+// see also: num-format
+
+const MILLION: f64 = 1_000_000.0;
+
+pub fn human_number(num: usize) -> String {
+    match (num as f64).log10().floor() as usize {
+        0..=2 => num.to_string(),
+        3..=5 => format!("{} K", num / 1000),
+        // 3 significant figures
+        6..=8 => format!("{} M", {
+            let num = ((num as f64) / MILLION).to_string();
+            num[..num.len().min(4)].to_string().trim_end_matches('0')
+        }),
+        _ => todo!(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::human_number;
+
+    #[test]
+    fn test_human_number() {
+        for (num, str) in [
+            (1, "1"),
+            (2, "2"),
+            (10, "10"),
+            (100, "100"),
+            (1000, "1 K"),
+            (10000, "10 K"),
+            (999999, "999 K"),
+            (1_000_000, "1 M"),
+            (1_100_000, "1.1 M"),
+            (1_100_001, "1.1 M"),
+        ] {
+            assert_eq!(human_number(num), str)
+        }
+    }
+}
